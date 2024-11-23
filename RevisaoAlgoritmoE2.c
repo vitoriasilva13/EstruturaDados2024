8cfabfefa42de22430254222dc6898c4ef650a17
Vitória Silva <silva.lopes.vitoria@gmail.com>
2024-11-23 18:19:35 -0300

    Add files via upload

---
+++ RevisaoAlgoritmoE2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*2. Fazer um programa para receber 3 valores inteiros do usuário e
+ * mostrar a sua média (que pode não ser inteira).
+*/
+
+int main()
+{
+    int numero;
+    
+    printf("Informe um número:");
+    scanf("%d", &numero);
+    
+    if (numero%2 > 0)
+        printf("O número informado é ímpar.");
+    else
+        printf("O número informado é par.");
+
+    return 0;
+}
